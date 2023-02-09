091a28056dbdef8b63d7ffa31d00d148cbe4f979
Flandre3569 <1332726596@qq.com>
2023-02-09 21:53:44 +0800

    HMR

--- src/node/plugin/indexHtml.ts
+++ src/node/plugin/indexHtml.ts
@@ -11,6 +11,7 @@ export function pluginIndexHtml(): Plugin {
         server.middlewares.use(async (req, res, next) => {
           let html = await readFile(DEFAULT_HTML_PATH, "utf-8");
           try {
+            html = await server.transformIndexHtml(req.url, html, req.originalUrl);
             res.statusCode = 200;
             res.setHeader("Content-Type", "text/html");
             res.end(html);
