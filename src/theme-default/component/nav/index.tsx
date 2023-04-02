fe27088bf09a05afdaaed0332a4dc84ee6b7e1db
Flandre3569 <1332726596@qq.com>
2023-04-02 22:28:07 +0800

    更改部分unocss错误写法

--- src/theme-default/component/nav/index.tsx
+++ src/theme-default/component/nav/index.tsx
@@ -17,7 +17,7 @@ export function Nav() {
   const { siteData } = useContextData();
   const nav = siteData?.themeConfig?.nav || [];
   return (
-    <header relative="~" w="full">
+    <header fixed="~" w="full">
       <div flex="~" justify="between" items="center" className="px-8 h-14 divider-bottom">
         <div>
           <a href="/" className="w-full h-full text-1rem flex items-center" hover="opacity-60">vigor.js</a>
