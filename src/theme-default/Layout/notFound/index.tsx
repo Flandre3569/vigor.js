84167542699d0cd214fb687475bd08853dc4bf62
Flandre3569 <1332726596@qq.com>
2023-04-10 21:10:30 +0800

    给404页面添加返回主页按钮

--- src/theme-default/Layout/notFound/index.tsx
+++ src/theme-default/Layout/notFound/index.tsx
@@ -5,6 +5,20 @@ export function NotFoundPage() {
     <div flex="~" h="full">
       <div w="1/2" h="full">
         <h1 className={styles.h1}>404</h1>
+        <a
+          inline-block=""
+          border="1px solid brand"
+          rounded="2xl"
+          p="y-2 x-4"
+          text="sm brand"
+          font-medium=""
+          href={'/'}
+          aria-label="go to home"
+          relative="~"
+          className="left-58 top-20"
+        >
+          Take me home
+        </a>
       </div>
       <div relative="~" className="top-10" >
         <img className="w-250 left-0" src="../../../../docs/public/404.png" alt="404" />
